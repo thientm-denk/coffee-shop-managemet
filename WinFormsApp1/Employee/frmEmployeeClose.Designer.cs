3d58291efe7fcb9ae256d2177c725d0f8a3ac431
Đỗ Thành Bộ <ronalbo2610@gmail.com>
2022-10-25 16:49:03 +0700

    add to branch

---
+++ WinFormsApp1/Employee/frmEmployeeClose.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace WinFormsApp1.Employee
+{
+    partial class frmEmployeeClose
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBefore = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtAfter = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtExpected = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(74, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(184, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Money before Shirf:";
+            // 
+            // txtBefore
+            // 
+            this.txtBefore.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtBefore.Location = new System.Drawing.Point(264, 55);
+            this.txtBefore.Name = "txtBefore";
+            this.txtBefore.Size = new System.Drawing.Size(282, 34);
+            this.txtBefore.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(74, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(167, 28);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Money after Shirf:";
+            // 
+            // txtAfter
+            // 
+            this.txtAfter.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtAfter.Location = new System.Drawing.Point(264, 109);
+            this.txtAfter.Name = "txtAfter";
+            this.txtAfter.Size = new System.Drawing.Size(282, 34);
+            this.txtAfter.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(74, 164);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(161, 28);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Money expected:";
+            // 
+            // txtExpected
+            // 
+            this.txtExpected.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtExpected.Location = new System.Drawing.Point(264, 161);
+            this.txtExpected.Name = "txtExpected";
+            this.txtExpected.Size = new System.Drawing.Size(282, 34);
+            this.txtExpected.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(80, 224);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(490, 19);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "*If the money in cashbox is more than expected, in will be added to your salary";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(80, 252);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(123, 19);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "*Have a good day!";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(409, 290);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 47);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Finish";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // frmEmployeeClose
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(616, 380);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtExpected);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtAfter);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtBefore);
+            this.Controls.Add(this.label1);
+            this.Name = "frmEmployeeClose";
+            this.Text = "v";
+            this.Load += new System.EventHandler(this.frmEmployeeClose_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox txtBefore;
+        private Label label2;
+        private TextBox txtAfter;
+        private Label label3;
+        private TextBox txtExpected;
+        private Label label4;
+        private Label label5;
+        private Button button1;
+    }
+}
---
+++ WinFormsApp1/Employee/frmNewOrder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1.Employee
+{
+    public partial class frmNewOrder : Form
+    {
+        public frmNewOrder()
+        {
+            InitializeComponent();
+        }
+    }
+}
